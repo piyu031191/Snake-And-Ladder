a7dfa590cbbd20ede2e1029cdf7e6ebde345d093
piyu031191 <priyankasambatkar0864@gmail.com>
2021-11-06 15:56:50 +0530

    starting position of both players

---
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -0,0 +1,13 @@
+package SnakeAndLadder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		int position1 = 0;
+		int position2 = 0;
+		System.out.println("Welcome to snake and ladder game:-");
+		System.out.println("Starting position of first player is :" + position1);
+	    System.out.println("Starting position of second player is :" + position2);
+	}
+
+}
