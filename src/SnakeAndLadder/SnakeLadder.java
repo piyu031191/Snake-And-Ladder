55067c3a0e2be87064e3c0b99455f0e2204bf287
piyu031191 <priyankasambatkar0864@gmail.com>
2021-11-06 16:18:57 +0530

    roll the die program

--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -5,9 +5,10 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		int position1 = 0;
 		int position2 = 0;
+		int diceCopy = (int) Math.floor( (Math.random() * 10) % 6 + 1 );
 		System.out.println("Welcome to snake and ladder game:-");
-		System.out.println("Starting position of first player is :" + position1);
-	    System.out.println("Starting position of second player is :" + position2);
+		System.out.println("Starting position of first player is :" + position1+" "+ diceCopy);
+	    System.out.println("Starting position of second player is :" + position2+" "+ diceCopy);
 	}
 
 }
