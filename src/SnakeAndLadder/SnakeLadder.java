c0915fbe9eb0cb597d864ae804683bd9eb7c0fcb
piyu031191 <priyankasambatkar0864@gmail.com>
2021-11-06 21:58:23 +0530

    updated snake and ladder program

--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -20,7 +20,8 @@ public static void main(String[] args) {
 	int position1 = 0;
 	int position2 = 0; 
 	int chance = 0;		
-	
+	int count1 =1;	
+	int count2 =1;
 	System.out.println("Welcome to snake and ladder game:-");
 	System.out.println("Dice roll by first player");
 	
@@ -32,29 +33,36 @@ public static void main(String[] args) {
 	    switch(option) {
 	    case 0: 
 	    	if(chance==0) {
-    	    	System.out.println("First player action : nothing");    	    		
+    	    	System.out.println("First player action : nothing");
+    	    	  count1 += 1;	
+    
 	    	}else {
     	    	System.out.println("Second player action : nothing");
+    	    	count2 += 1;
 	    	}
 	    	break;
 	    case 1: 
-	    //SnakeLadder.Action(position2, diceCopy2, null)
+	   
 	    	if(chance==0) {
 	    		position1 = SnakeLadder.Action(position1, diceCopy, "ladder");
-	    		System.out.println("First player action: ladder with Position " + position1); 		
+	    		System.out.println("First player action: ladder with Position " + position1);
+	    		count1 += 1;	
 	    	}else {
 	    		position2 = SnakeLadder.Action(position2, diceCopy, "ladder");
-	    		System.out.println("Second player action: ladder with Position " + position2);		
+	    		System.out.println("Second player action: ladder with Position " + position2);
+	    		count2 += 1;	
 	    	}
 	    	break;
 	    case 2: 
 	    	System.out.println(" Player action: snake");
 	    	if(chance==0) {
 	    		position1 = SnakeLadder.Action(position1, diceCopy, "snake");
-	    		System.out.println("First player action: snake with Position " + position1); 		
+	    		System.out.println("First player action: snake with Position " + position1); 
+	    		count1 += 1;	
 	    	}else {
 	    		position2 = SnakeLadder.Action(position2, diceCopy, "snake");
-	    		System.out.println("Second player action: snake with Position " + position2);		
+	    		System.out.println("Second player action: snake with Position " + position2);
+	    		count2 += 1;	
 	    	}
 	    	break;	    	
     	    }
